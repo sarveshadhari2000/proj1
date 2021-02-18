6885f806309ac2777410744ef0638e510478617e
sarveshadhari2000 <78435435+sarveshadhari2000@users.noreply.github.com>
2021-02-18 17:57:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,144 +10,83 @@ import { BoxLineGeometry } from './libs/three/jsm/BoxLineGeometry.js';
 import { Stats } from './libs/stats.module.js';
 import { ARButton } from "./libs/ARButton.js"
 
-class App
-{
-    constructor ()
-    {
-        const container = document.createElement('div');
-        document.body.appendChild(container);
-
-        this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100);
-        this.camera.position.set(0,0,4);
-
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color(0xaaaaaa);
-
-        this.renderer = new THREE.WebGLRenderer({antialias:true});
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(window.innerWidth,window.innerHeight);
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
         
-        container.appendChild(this.renderer.domElement);
-
+        this.clock = new THREE.Clock();
         
+		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+		
+		this.scene = new THREE.Scene();
        
+		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
 
-        this.ambient = new THREE.AmbientLight(0xffffff,0.5);
-        this.scene.add(this.ambient);
-
-        this.pointLight = new THREE.DirectionalLight(0xffffff,1);
-        this.scene.add(this.pointLight);
-
-        this.pointLight.position.set(1,1,1);
-
-
-        const controls = new OrbitControls(this.camera,this.renderer.domElement);
+        const light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 1, 1, 1 ).normalize();
+		this.scene.add( light );
+			
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        
+		container.appendChild( this.renderer.domElement );
+        
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+        this.controls.target.set(0, 3.5, 0);
+        this.controls.update();
+        
+        this.stats = new Stats();
+        
         this.initScene();
-        this.setupAR();
-
-        window.addEventListener('resize',this.resize.bind(this))
+        this.setupVR();
+        
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    initScene(){
+        this.geometry = new THREE.BoxBufferGeometry( 0.06, 0.06, 0.06 ); 
+        this.meshes = [];
     }
-
-    random( min, max ){
-        return Math.random() * (max-min) + min;
-    }
-
-    initScene()
-    {
-            this.geometry = new THREE.BoxBufferGeometry(0.06,0.06,0.06);
-            this.meshes = [];
-    }
-
-    setupAR()
-    {
-        this.renderer.xr.enabled = true;
-        let controller;
+    
+    setupVR(){
+        this.renderer.xr.enabled = true; 
+        
         const self = this;
+        let controller;
+        
+        function onSelect() {
+            const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+            const mesh = new THREE.Mesh( self.geometry, material );
+            mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+            mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+            self.scene.add( mesh );
+            self.meshes.push( mesh );
 
-        function onSelect()
-        {
-            const material = new THREE.MeshPhongMaterial({color:(0xffffff*Math.random())});
-            const mesh = new THREE.Mesh(this.geometry,material);
-            mesh.position.set(0,0,-0.3).applyMatrix4(controller.matrixWorld);
-            mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
-            this.scene.add(mesh);
-            this.meshes.add(mesh);
         }
 
-        const btn = new ARButton(this.renderer);
-
-        controller = this.renderer.xr.getController(0);
-        controller.addEventListener('select',onSelect);
-        this.scene.add(controller);
-        this.renderer.setAnimationLoop(this.render.bind(this));
-
-    }
-
-    resize()
-    {
-           this.camera.aspect = window.innerWidth/window.innerHeight;
-           this.camera.updateProjectionMatrix();
-           this.renderer.setSize(window.innerWidth,window.innerHeight);
-    }
-
-    render()
-    {
-        this.meshes.forEach((mesh)=>{mesh.rotateY(0.02);});
-        this.renderer.render(this.scene,this.camera);
-    }
-
-    loadGLTF()
-    {
-        const self = this;
-        const loader = new GLTFLoader().setPath("./assets/")
-
-        loader.load(
-            'office-chair.glb',
-            function(gltf)
-            {
-                self.chair = gltf.scene;
-                self.scene.add(gltf.scene);
-                self.loadingBar.visible = false;
-                self.renderer.setAnimationLoop(self.render.bind(self));
-            },
-            function(xhr)
-            {
-               self.loadingBar.progress = xhr.loaded/xhr.total;
-            },
-            function(err)
-            {
-                console.log("Error");
-            }
-
-        )
+        const btn = new ARButton( this.renderer );
         
+        controller = this.renderer.xr.getController( 0 );
+        controller.addEventListener( 'select', onSelect );
+        this.scene.add( controller );
+        
+        this.renderer.setAnimationLoop( this.render.bind(this) );
     }
-
-    loadFBX()
-    {
-        const self = this;
-        const loader = new FBXLoader().setPath("./assets/")
-
-        loader.load(
-            'office-chair.fbx',
-            function(object)
-            {
-                self.chair = object;
-                self.scene.add(object);
-                self.loadingBar.visible = false;
-                self.renderer.setAnimationLoop(self.render.bind(self));
-            },
-            function(xhr)
-            {
-               self.loadingBar.progress = xhr.loaded/xhr.total;
-            },
-            function(err)
-            {
-                console.log("Error");
-            }
-
-        )
+    
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+    
+	render( ) {   
+        this.stats.update();
+        this.meshes.forEach( (mesh) => { mesh.rotateY( 0.01 ); });
+        this.renderer.render( this.scene, this.camera );
     }
 }
 
-export {App};
+export { App };
