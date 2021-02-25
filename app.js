508f29b376eb9630ee88d9f568977fffe4fee120
sarveshadhari2000 <78435435+sarveshadhari2000@users.noreply.github.com>
2021-02-25 21:49:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-import * as THREE from './libs/three/three.module.js';
-import { VRButton } from './VRButton.js';
+import * as THREE from "./libs/three/three.module.js"
+import {OrbitControls} from "./libs/three/jsm/OrbitControls.js"
+import {GLTFLoader} from "./libs/three/jsm/GLTFLoader.js"
+import {FBXLoader} from "./libs/three/jsm/FBXLoader.js"
+import {LoadingBar} from "./libs/LoadingBar.js"
+import {vector3ToString} from "./libs/DebugUtils.js"
+import { VRButton } from './libs/three/jsm/VRButton.js';
 import { XRControllerModelFactory } from './libs/three/jsm/XRControllerModelFactory.js';
 import { BoxLineGeometry } from './libs/three/jsm/BoxLineGeometry.js';
 import { Stats } from './libs/stats.module.js';
-import { OrbitControls } from './libs/three/jsm/OrbitControls.js';
-import { Player } from './libs/Player.js';
-import { ControllerGestures } from './libs/ControllerGestures.js';
-import { LoadingBar } from './libs/LoadingBar.js';
-import { GLTFLoader } from './libs/three/jsm/GLTFLoader.js';
-import { CanvasUI } from './libs/CanvasUI.js'
-import { ARButton } from './libs/ARButton.js';
-import { RGBELoader } from './libs/three/jsm/RGBELoader.js';
-
+import { ARButton } from "./libs/ARButton.js"
 
 class App{
 	constructor(){
@@ -20,221 +17,75 @@ class App{
         
         this.clock = new THREE.Clock();
         
-        this.loadingBar = new LoadingBar();
-
-		this.assetsPath = './assets/';
-        
 		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
-		this.camera.position.set( 0, 1.6, 3 );
-        
+		
 		this.scene = new THREE.Scene();
+       
+		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
 
-		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 2);
-        ambient.position.set( 0.5, 1, 0.25 );
-		this.scene.add(ambient);
-        
-        const light = new THREE.DirectionalLight();
-        light.position.set( 0.2, 1, 1);
-        this.scene.add(light);
+        const light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 1, 1, 1 ).normalize();
+		this.scene.add( light );
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
-		this.renderer.outputEncoding = THREE.sRGBEncoding;
-		container.appendChild( this.renderer.domElement );
-        this.setEnvironment();
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
         
-        this.workingVec3 = new THREE.Vector3();
+		container.appendChild( this.renderer.domElement );
+        
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+        this.controls.target.set(0, 3.5, 0);
+        this.controls.update();
+        
+        this.stats = new Stats();
         
         this.initScene();
-        this.setupXR();
-		
-		window.addEventListener('resize', this.resize.bind(this));
+        this.setupVR();
         
-	}
-    
-    setEnvironment(){
-        const loader = new RGBELoader().setDataType( THREE.UnsignedByteType );
-        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
-        pmremGenerator.compileEquirectangularShader();
-        
-        const self = this;
-        
-        loader.load( './assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
-          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-          pmremGenerator.dispose();
-
-          self.scene.environment = envMap;
-
-        }, undefined, (err)=>{
-            console.error( 'An error occurred setting the environment');
-        } );
-    }
-	
-    resize(){ 
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-    	this.camera.updateProjectionMatrix();
-    	this.renderer.setSize( window.innerWidth, window.innerHeight );  
-    }
-    
-    loadKnight(){
-	    const loader = new GLTFLoader().setPath(this.assetsPath);
-		const self = this;
-		
-		// Load a GLTF resource
-		loader.load(
-			// resource URL
-			`knight2.glb`,
-			// called when the resource is loaded
-			function ( gltf ) {
-				const object = gltf.scene.children[5];
-				
-				const options = {
-					object: object,
-					speed: 0.5,
-					assetsPath: self.assetsPath,
-					loader: loader,
-                    animations: gltf.animations,
-					clip: gltf.animations[0],
-					app: self,
-					name: 'knight',
-					npc: false
-				};
-				
-				self.knight = new Player(options);
-                self.knight.object.visible = false;
-				
-				self.knight.action = 'Dance';
-				const scale = 0.005;
-				self.knight.object.scale.set(scale, scale, scale); 
-				
-                self.loadingBar.visible = false;
-                self.renderer.setAnimationLoop( self.render.bind(self) );//(timestamp, frame) => { self.render(timestamp, frame); } );
-			},
-			// called while loading is progressing
-			function ( xhr ) {
-
-				self.loadingBar.progress = (xhr.loaded / xhr.total);
-
-			},
-			// called when loading has errors
-			function ( error ) {
-
-				console.log( 'An error happened' );
-
-			}
-		);
-	}		
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
     
     initScene(){
-        this.reticle = new THREE.Mesh(
-            new THREE.RingBufferGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
-            new THREE.MeshBasicMaterial()
-        );
-        
-        this.reticle.matrixAutoUpdate = false;
-        this.reticle.visible = false;
-        this.scene.add( this.reticle );
-        
-        this.loadKnight();
+        this.geometry = new THREE.BoxBufferGeometry( 0.06, 0.06, 0.06 ); 
+        this.meshes = [];
     }
     
-    setupXR(){
-        this.renderer.xr.enabled = true;
-        
-        const btn = new ARButton( this.renderer, { sessionInit: { requiredFeatures: [ 'hit-test' ], optionalFeatures: [ 'dom-overlay' ], domOverlay: { root: document.body } } } );
+    setupVR(){
+        this.renderer.xr.enabled = true; 
         
         const self = this;
-
-        this.hitTestSourceRequested = false;
-        this.hitTestSource = null;
+        let controller;
         
         function onSelect() {
-            if (self.knight===undefined) return;
-            
-            if (self.reticle.visible){
-                if (self.knight.object.visible){
-                    self.workingVec3.setFromMatrixPosition( self.reticle.matrix );
-                    self.knight.newPath(self.workingVec3);
-                }else{
-                    self.knight.object.position.setFromMatrixPosition( self.reticle.matrix );
-                    self.knight.object.visible = true;
-                }
-            }
+            const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+            const mesh = new THREE.Mesh( self.geometry, material );
+            mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+            mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+            self.scene.add( mesh );
+            self.meshes.push( mesh );
+
         }
 
-        this.controller = this.renderer.xr.getController( 0 );
-        this.controller.addEventListener( 'select', onSelect );
+        const btn = new ARButton( this.renderer );
         
-        this.scene.add( this.controller );    
+        controller = this.renderer.xr.getController( 0 );
+        controller.addEventListener( 'select', onSelect );
+        this.scene.add( controller );
+        
+        this.renderer.setAnimationLoop( this.render.bind(this) );
     }
     
-    requestHitTestSource(){
-        const self = this;
-        
-        const session = this.renderer.xr.getSession();
-
-        session.requestReferenceSpace( 'viewer' ).then( function ( referenceSpace ) {
-            
-            session.requestHitTestSource( { space: referenceSpace } ).then( function ( source ) {
-
-                self.hitTestSource = source;
-
-            } );
-
-        } );
-
-        session.addEventListener( 'end', function () {
-
-            self.hitTestSourceRequested = false;
-            self.hitTestSource = null;
-            self.referenceSpace = null;
-
-        } );
-
-        this.hitTestSourceRequested = true;
-
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
     }
     
-    getHitTestResults( frame ){
-        const hitTestResults = frame.getHitTestResults( this.hitTestSource );
-
-        if ( hitTestResults.length ) {
-            
-            const referenceSpace = this.renderer.xr.getReferenceSpace();
-            const hit = hitTestResults[ 0 ];
-            const pose = hit.getPose( referenceSpace );
-
-            this.reticle.visible = true;
-            this.reticle.matrix.fromArray( pose.transform.matrix );
-
-        } else {
-
-            this.reticle.visible = false;
-
-        }
-
-    }
-
-    render( timestamp, frame ) {
-        const dt = this.clock.getDelta();
-        if (this.knight) this.knight.update(dt);
-
-        const self = this;
-        
-        if ( frame ) {
-
-            if ( this.hitTestSourceRequested === false ) this.requestHitTestSource( )
-
-            if ( this.hitTestSource ) this.getHitTestResults( frame );
-
-        }
-
+	render( ) {   
+        this.stats.update();
+        this.meshes.forEach( (mesh) => { mesh.rotateY( 0.01 ); });
         this.renderer.render( this.scene, this.camera );
-        
-        /*if (this.knight.calculatedPath && this.knight.calculatedPath.length>0){
-            console.log( `path:${this.knight.calculatedPath[0].x.toFixed(2)}, ${this.knight.calculatedPath[0].y.toFixed(2)}, ${this.knight.calculatedPath[0].z.toFixed(2)} position: ${this.knight.object.position.x.toFixed(2)}, ${this.knight.object.position.y.toFixed(2)}, ${this.knight.object.position.z.toFixed(2)}`);
-        }*/
     }
 }
 
